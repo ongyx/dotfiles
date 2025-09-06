42f67564e59f2d7e9721810ebe4c7eedbdc58a3e
Ong Yong Xin <ongyongxin2020@gmail.com>
2025-09-06 12:38:06 +0800

    feat: Add windows config to setup.py feat: Add Windows Terminal settings

--- setup.py
+++ setup.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python
-import pathlib
+import os
+from pathlib import Path
 
-LINKS = {
-    "ghostty": "~/.config/ghostty",
-    "helix": "~/.config/helix",
-    "yt-dlp": "~/.config/yt-dlp",
-    "zsh": "~/.config/zsh",
-    ".zshrc": "~/.zshrc",
-    ".zshenv": "~/.zshenv",
-    ".tmux.conf": "~/.tmux.conf"
-}
+
+def get_links() -> dict[str, Path]:
+    match os.name:
+        case "nt":
+            local_app_data = os.environ["LOCALAPPDATA"]
+            roaming_app_data = os.environ["APPDATA"]
+
+            return {
+                "helix": Path(rf"{roaming_app_data}\helix"),
+                r"helix\config.windows.toml": Path(r"helix\config.toml").absolute(),
+                "yt-dlp": Path(rf"{roaming_app_data}\yt-dlp"),
+                "wt": Path(
+                    rf"{local_app_data}\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+                ),
+            }
+
+        case "posix":
+            home = os.environ["HOME"]
+
+            return {
+                "ghostty": Path(f"{home}/.config/ghostty"),
+                "helix": Path(f"{home}/.config/helix"),
+                r"helix\config.posix.toml": Path(r"helix\config.toml").absolute(),
+                "yt-dlp": Path(f"{home}/.config/yt-dlp"),
+                "zsh": Path(f"{home}/.config/zsh"),
+                ".zshrc": Path(f"{home}/.zshrc"),
+                ".zshenv": Path(f"{home}/.zshenv"),
+                ".tmux.conf": Path(f"{home}/.tmux.conf"),
+            }
+
+        case _:
+            return {}
+
 
 def main():
-    for target, link in LINKS.items():
-        target_path = pathlib.Path(target).absolute()
-        link_path = pathlib.Path(link).expanduser()
+    for target, link_path in get_links().items():
+        target_path = Path(target).absolute()
 
         if link_path.is_symlink():
             print(f"Removing symlink at {link_path}")
@@ -25,7 +49,7 @@ def main():
             continue
 
         print(f"Symlinking {target_path} to {link_path}")
-        link_path.symlink_to(target_path, target_is_directory=True)
+        link_path.symlink_to(target_path)
 
 
 if __name__ == "__main__":
